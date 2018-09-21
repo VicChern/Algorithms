af05b1f95da41e9803a4ff5fbf58112e7573fc44
VicChern <32546828+VicChern@users.noreply.github.com>
2018-09-21 18:18:12 +0300

    Update FindChar.java

--- FindChar.java
+++ FindChar.java
@@ -1,5 +1,7 @@
 package com.company;
 
+// Given a string s, find and return the first instance of a non-repeating character in it. 
+// If there is no such character, return '_'.
 
 public class FindChar {
 
@@ -8,15 +10,18 @@ public class FindChar {
         firstNotRepeatingCharacter(s);
     }
 
+   
    static char firstNotRepeatingCharacter(String s) {
+       
+// converting string to array for checking every character
         char[] c=s.toCharArray();
         for(int i=0;i<s.length();i++){
+//this statement compareTo equality our characters in string 
             if(s.indexOf(c[i])==s.lastIndexOf(c[i]))
                 return c[i];
         }
         return '_';
     }
-
 }
 
 
